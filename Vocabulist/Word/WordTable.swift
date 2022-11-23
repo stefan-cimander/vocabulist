c591b192edfbb3d10031059b731b80b541ca991b
Stefan Cimander <stefan.cimander@me.com>
2022-11-23 23:20:08 +0100

    #1 Make word table sortable and add creation date column

--- Vocabulist/Word/WordTable.swift
+++ Vocabulist/Word/WordTable.swift
@@ -11,13 +11,26 @@ struct WordTable: View {
     
     var words: [Word]
     
+    private var sortedWords: [Word] {
+        words.sorted(using: sortOrder)
+    }
+    
     @State private var selection = Set<Word.ID>()
+    @State private var sortOrder = [KeyPathComparator(\Word.creationDate!)]
     
     var body: some View {
-        Table(words, selection: $selection) {
-            TableColumn("Spanish") { Text($0.foreignName ?? "") }
-            TableColumn("German") { Text($0.nativeName ?? "") }
+        Table(sortedWords, selection: $selection, sortOrder: $sortOrder) {
+            TableColumn("Spanish", value: \.foreignName!)
+            TableColumn("German", value: \.nativeName!)
+            TableColumn("Created", value: \.creationDate!) { Text($0.creationDate!, formatter: dateFormatter) }
             TableColumn("Level", value: \.level.description).width(min: 50, max: 60)
         }
     }
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
 }
